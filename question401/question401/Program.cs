9dbaacdc640e56c7d80ee33b63f4981136be7747
Jotaro_Aso <j.asou@make-tomorrow.com>
2026-06-29 16:48:18 +0900

    ~quesiton401

---
+++ question401/question401/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace question401;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //インスタンスの生成
+        Rectangle r = new Rectangle();
+        //図形の幅と高さを入力
+        Console.Write("幅(cm)");
+        r.width = double.Parse(Console.ReadLine());
+        Console.Write("高さ(cm)");
+        r.height = double.Parse(Console.ReadLine());
+        //面積の表示
+        Console.WriteLine($"面積:{r.GetArea()}cm2");
+        Console.WriteLine($"周の長さ:{r.GetPerimeter()}cm");
+    }
+}
---
+++ question401/question401/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace question401;
+
+class Rectangle
+{
+    //フィールドの設定
+    public double width; 
+    public double height;
+
+    //メソッドの設定
+    public double GetArea()
+    {
+        return width * height;
+    }
+    public double GetPerimeter()
+    {
+        return width * 2 + height * 2;
+    }
+}
