16859d5bccfb0cedcb99b2fefd51aa096c8d9147
Jotaro_Aso <j.asou@make-tomorrow.com>
2026-06-29 15:57:13 +0900

    sample401完了

---
+++ sample401/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace BookStudy;
+
+class Person
+{
+    //名前(フィールド)
+    public string name = "";
+    //年齢(フィールド)
+    public int age = 0;
+    //情報の表示(メソッド)
+    public void ShowAgeAndName()
+    {
+        Console.WriteLine($"名前:{name} 年齢:{age}");
+    }
+    //情報の設定(メソッド)
+    public void SetAgeAndName(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
