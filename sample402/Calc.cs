be683bdd4e0bdd103ec67d3db9beab11f305fe29
Jotaro_Aso <j.asou@make-tomorrow.com>
2026-06-29 16:48:02 +0900

    ~sample402

---
+++ sample402/Calc.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStudy;
+
+class Calc
+{
+    //2つの整数の引数の和を求める
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    //3つの整数の引数の和を求める
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
