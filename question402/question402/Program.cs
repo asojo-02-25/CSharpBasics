03ce41f25fa03a5fc587eddcee3d501656017bec
Jotaro Aso <jotaro.aso@gmail.com>
2026-07-01 12:54:24 +0900

    問題402完了

---
+++ question402/question402/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace question402;
+
+class Program
+{
+    static void Main(string[] arg)
+    {
+        Console.Write("文字を入力してください。");
+        string str = Console.ReadLine() ?? "";
+
+        Console.WriteLine($"大文字:{str.ToUpper()}");
+        Console.WriteLine($"小文字:{str.ToLower()}");
+    }
+}
