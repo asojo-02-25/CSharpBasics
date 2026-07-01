872d974950ea0ffea4c11f3e87fbe090f92499b6
Jotaro Aso <jotaro.aso@gmail.com>
2026-07-01 12:53:10 +0900

    303,304完了

---
+++ example303/example303/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace example303;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("数あてゲーム");
+
+        Random rnd = new Random();
+        int answer = rnd.Next(1, 11);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("0から10までの値を入力してください。");
+            string input = Console.ReadLine() ?? "";
+
+            if (!int.TryParse(input, out int number))
+            {
+                Console.WriteLine("無効な値です。");
+                return;
+            }
+
+            if (number > answer)
+            {
+                Console.WriteLine("大きすぎます。");
+
+            }
+            else if (number < answer)
+            {
+                Console.WriteLine("小さすぎます。");
+            }
+            else
+            {
+                Console.WriteLine("正解です！");
+                return;
+            }
+        }
+        Console.WriteLine("ゲームオーバー！");
+        Console.WriteLine($"正解は{answer}です。");
+    }
+}
---
+++ example304/example304/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rnd = new Random();
+int max = 0;
+int min = 100;
+
+for (int i = 0; i < 10; i++)
+{
+    int num = rnd.Next(1, 101);
+    Console.Write(num + " ");
+
+    if (num > max)
+    {
+        max = num;
+    }
+    else if (num < min)
+    {
+        min = num;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"最大値:{max}");
+Console.WriteLine($"最小値:{min}");
