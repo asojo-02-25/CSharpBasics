7dd8bc06cb1c249f34fdc283966671b9dcd0f618
Jotaro_Aso <j.asou@make-tomorrow.com>
2026-07-02 13:16:44 +0900

    401, 402復習完了

--- Bookstudy/question402/question402/Program.cs
+++ Bookstudy/question402/question402/Program.cs
@@ -2,12 +2,12 @@
 
 class Program
 {
-    static void Main(string[] arg)
+    static void Main(string[] args)
     {
-        Console.Write("文字を入力してください。");
-        string str = Console.ReadLine() ?? "";
+        Console.Write("英文を入力してください : ");
+        string text = Console.ReadLine()!;
 
-        Console.WriteLine($"大文字:{str.ToUpper()}");
-        Console.WriteLine($"小文字:{str.ToLower()}");
+        Console.WriteLine($"大文字 : {text.ToUpper()}");
+        Console.WriteLine($"小文字 : {text.ToLower()}");
     }
 }
