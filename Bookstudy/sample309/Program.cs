9af41a15901d11a861931f756679755abdca82c5
Jotaro_Aso <j.asou@make-tomorrow.com>
2026-07-02 13:31:10 +0900

    ~sample309

---
+++ Bookstudy/sample309/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace sample309;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] a = new int[3, 4];
+        
+        for (int m = 0; m < 3; m++)
+        {
+            for (int n = 0; n < 4; n++)
+            {
+                a[m, n] = m + n;
+            }
+        }
+
+        for (int m = 0; m < 3; m++)
+        {
+            for (int n = 0; n < 4; n++)
+            {
+                Console.WriteLine($"a[{m},{n}] = {m + n}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
